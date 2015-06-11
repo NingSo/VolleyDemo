376818df5ed7083665087317a2141603115ad979
ningpingping <ningpingping@tifen.com>
2015-06-11 18:43:18 +0800

    no message

---
+++ app/src/main/java/com/ningso/volleydemo/Fragments/ImageLoadFragment.java
@@ -0,0 +1,7 @@
+package com.ningso.volleydemo.Fragments;
+
+/**
+ * Created by NingPingPing on 2015/6/10.
+ */
+public class ImageLoadFragment {
+}
---
+++ app/src/main/java/com/ningso/volleydemo/Fragments/NormalPostRequestFragment.java
@@ -0,0 +1,9 @@
+package com.ningso.volleydemo.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by NingPingPing on 2015/6/10.
+ */
+public class NormalPostRequestFragment extends Fragment {
+}
